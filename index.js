5985d45aa1c304a8d628382d017b3e698e81d2c3
Braiton32 <braitononer32@gmail.com>
2026-01-07 22:08:16 -0600

    cuarto commit

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('trust proxy', true);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const userIP = req.ip;
+    const fecha = new Date().toLocaleString();
+    
+    console.log(`[${fecha}] Nueva visita desde la IP: ${userIP}`);
+
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor activo en puerto ${port}`);
+});
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
