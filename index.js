952fd0151684d2169cce21b762dcac7f11c6ce32
Braiton32 <braitononer32@gmail.com>
2026-01-07 22:42:31 -0600

    quinto commit

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('trust proxy', true);
+// ESTO DEBE IR ARRIBA
+app.set('trust proxy', true); 
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    const userIP = req.ip;
-    const fecha = new Date().toLocaleString();
-    
-    console.log(`[${fecha}] Nueva visita desde la IP: ${userIP}`);
-
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // Prueba con estas dos opciones si req.ip falla
+    const ip = req.headers['x-forwarded-for'] || req.ip;
+    console.log("IP DETECTADA:", ip);
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
